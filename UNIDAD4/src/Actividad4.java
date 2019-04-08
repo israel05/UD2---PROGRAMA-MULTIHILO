9f673ee0334a518fbd2b658b2693fb44ca05117a
Israel de Lamo Blas <i.delamo@gmail.com>
2019-04-08 23:15:40 +0100

    cambio de letra

--- UNIDAD4/src/Actividad4.java
+++ UNIDAD4/src/Actividad4.java
@@ -8,6 +8,7 @@ public class Actividad4 {
 	 * 
 	 * En este ejercicio se puede ver como JavaDoc ayuda a la creacción de una
 	 * documentación ordenada que debe aparecer en todo buen programa
+	 * 
 	 * @author Israel de Lamo
 	 * @version 1.0
 	 * @see posicion.java
@@ -20,7 +21,7 @@ public class Actividad4 {
 		Thread p = new Thread(new Posicion("Primero"));
 		Thread s = new Thread(new Posicion("Segundo"));
 		p.start();
-		s.start();
+		s.start(); //nada mÃ¡s
 		System.out.println( "Fin programa ");
 	}
 }
